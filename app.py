d3751898206833133aca4c13765e35c326da936b
Tanmay Thakur <thakurtanmay72@yahoo.com>
2018-10-23 19:34:34 +0530

    Predict Scipt uploaded

--- app.py
+++ app.py
@@ -8,13 +8,11 @@ Created on Sun Oct 14 18:28:29 2018
 # Importing Modules
 import numpy as np
 import pandas as pd
-import random
 import re
 from keras.models import Sequential
 from keras.layers import Dropout
 from keras.callbacks import ModelCheckpoint
 from keras.layers import Dense, Activation, Bidirectional
-from keras.optimizers import RMSprop
 from keras.layers import CuDNNLSTM
 
 # Importing Data
@@ -77,28 +75,10 @@ model.fit(x, y, batch_size = 128, epochs = 10,callbacks = callbacks_list)
 
 # Model Perfoemance
 model.summary()
+
+# Saving the Model
 model.save('model.h5')
 
-#=====================================================================#
 
-# Predicting
-generated = ''
-start_index = random.randint(0,len(text)-maxlen-1)
-sent = text[start_index:start_index+maxlen]
-generated += sent
-for i in range(200):
-    x_sample = generated[i:i+maxlen]
-    x = np.zeros((1,maxlen,vocab_size))
-    for j in range(maxlen):
-        x[0,j,char_indices[x_sample[j]]] = 1
-    probs = model.predict(x)
-    probs = np.reshape(probs,probs.shape[1])
-    ix = np.random.choice(range(vocab_size),p=probs.ravel())
-    generated += indices_char[ix]
-
-print('Start')
-print(sent)		 
-print('Returning prediction...')
-print(generated)
    
 		    
