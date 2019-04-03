eec7007900d9cfa7731709d935a2273baa41cad7
Tanmay Thakur <thakurtanmay72@yahoo.com>
2019-04-03 12:06:29 +0530

    Custom Input Func Add

--- predict_webapp.py
+++ predict_webapp.py
@@ -7,6 +7,7 @@ Created on Tue Oct 23 19:16:30 2018
 # Importing Modules
 import numpy as np
 import pandas as pd
+import tensorflow as tf
 import random
 import re
 import flask
@@ -86,3 +87,22 @@ if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 5000))
 	print('Starting app...')
 	app.run(host='0.0.0.0', port=port)
+    
+def custom_predict(sent):
+    generated = ''
+    with graph.as_default():
+        while len(sent) < maxlen:
+        		  sent+=' '
+        generated+=sent.lower()
+        for i in range(200):
+    		    x_sample=generated[i:i+maxlen]
+    		    x=np.zeros((1,maxlen,vocab_size))
+    		    for j in range(maxlen):
+    		        x[0,j,char_indices[x_sample[j]]]=1
+    		    probs=model.predict(x)
+    		    probs=np.reshape(probs,probs.shape[1])
+    		    ix=np.random.choice(range(vocab_size),p=probs.ravel())
+    		    generated+=indices_char[ix]
+    return generated
+
+
